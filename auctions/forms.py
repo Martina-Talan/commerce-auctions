a957914fafddb423a090aa83defd6c04c1f4a2c1
Martina-Talan <talanmartina.de@gmail.com>
2025-04-03 22:13:24 +0200

    Update project

--- auctions/forms.py
+++ auctions/forms.py
@@ -4,18 +4,38 @@ from .models import Listing, Bids, Comment
 
 
 # Create a form 
+from django import forms
+from django.forms import ModelForm
+from .models import Listing
+
 class NewForm(ModelForm):
     class Meta:
         model = Listing
-        fields = ('title', 'description', 'starting_bid', 'image_url','category')
+        fields = ('title', 'description', 'starting_bid', 'image_url', 'category')
         widgets = {
-            "title": forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 450px;'}),
-            "description": forms.Textarea(attrs={'class': 'form-control', 'rows':3, 'style':'width: 450px;'}),
-            "starting_bid": forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 450px;'}),
-            "image_url": forms.TextInput(attrs={'class': 'form-control','style': 'width:450px;'}),
-            "category": forms.Select(attrs={'class': 'form-control','style': 'width: 450px;' }),
-
+            "title": forms.TextInput(attrs={
+               'placeholder': 'Enter title',
+               'style': 'width: 430px; max-width: 500px; outline:none;',
+            }),
+            "description": forms.Textarea(attrs={
+                'rows': 3,
+                'placeholder': 'Enter description',
+                'style': 'width: 430px; max-width:  500px; outline:none;',
+            }),
+            "starting_bid": forms.NumberInput(attrs={
+                'placeholder': 'Enter starting bid',
+                'style': 'width: 430px; max-width:    500px; padding: 15px !important; outline:none;',
+            }),
+            "image_url": forms.TextInput(attrs={
+                'placeholder': 'Enter image URL',
+                'style': 'width: 430px; max-width: 500px; outline:none;',
+            }),
+            "category": forms.Select(attrs={
+                'placeholder': 'Choose category',
+                'style': 'width: 430px; max-width: 500px; outline:none;',
+            }),
         }
+
 
 class BidForm(ModelForm):
     class Meta:
@@ -25,7 +45,7 @@ class BidForm(ModelForm):
             "current_bid": ("")
             }
          widgets ={
-         'current_bid': forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 450px;'}) 
+         'current_bid': forms.NumberInput(attrs={'style': 'width: 545px; outline:none;',}) 
       }
 
 
@@ -38,9 +58,9 @@ class CommentForm(ModelForm):
             }
         widgets = {
             "comment": forms.Textarea(attrs={
-                "placeholder": "Add your comment here",
-                "class": "form-control",
-                "rows": 1
+                'placeholder': "Add your comment here",
+                'style': 'width: 545px; outline:none;',
+                'rows': 1
             })
         }
 
